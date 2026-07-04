da8aa3c4a7add51699c9653d525407e6a0b9eb82
Pixlox <85137818+Pixlox@users.noreply.github.com>
2026-07-04 12:17:32 +1000

    lookup vals idk why theres a z

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Ballistics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Ballistics.java
@@ -6,8 +6,8 @@ import org.firstinspires.ftc.teamcode.utils.MathEx;
 
 @Configurable
 public class Ballistics {
-    public static double[] DISTANCE_LOOKUP = {0.71, 1.86, 3.2}; // metres
-    public static double[] SPEED_LOOKUP =    {37.0, 52.0, 70.1}; // rps
+    public static double[] DISTANCE_LOOKUP = {0.71, 1.86, 3.2, 0, 0, 0, 0}; // metres
+    public static double[] SPEED_LOOKUP =    {37.0, 52.0, 70.1, 0, 0, 0, 0}; // rps
 
     public static double calculateTurretAngle(double tagBearingDeg, double currentAngleDeg) {
         return currentAngleDeg + tagBearingDeg;
