e0300f235935591f859ca69f46ab5aea919158a8
SyntaxBreak <88933260+SyntaxBreak@users.noreply.github.com>
2026-07-04 10:16:55 +1000

    tune table + fix tick rate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Ballistics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Ballistics.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.bylazar.configurables.annotations.Configurable;
+
 import org.firstinspires.ftc.teamcode.utils.MathEx;
 
+@Configurable
 public class Ballistics {
-    private static double[] DISTANCE_LOOKUP = {0.71, 1.86, 3.27}; // metres
-    private static double[] SPEED_LOOKUP =    {37.0, 52.0, 70.0}; // rps
+    public static double[] DISTANCE_LOOKUP = {0.71, 1.86, 3.2}; // metres
+    public static double[] SPEED_LOOKUP =    {37.0, 52.0, 70.1}; // rps
 
     public static double calculateTurretAngle(double tagBearingDeg, double currentAngleDeg) {
         return currentAngleDeg + tagBearingDeg;
