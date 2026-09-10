0dcd50d56bde8b66a47e977fe54da5da425519f7
kinglob <der1010101013@gmail.com>
2026-09-10 15:21:27 +1000

    slight drivetrain changes
    Signed-off-by: kinglob <der1010101013@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,16 +1,13 @@
-package org.firstinspires.ftc.teamcode.subsystems;
+package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.teamcode.configs.TickRates.GOBILDA_5203_312RPM;
-
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.teamcode.configs.RobotHardware;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
-import org.firstinspires.ftc.teamcode.configs.RobotHardware;
 
 public class Drivetrain {
 
@@ -29,7 +26,6 @@ public class Drivetrain {
 
     private double speedMultiplier = SPEED_NORMAL;
 
-
     public Drivetrain(RobotHardware hw) {
         (flDrive = hw.flDrive).setDirection(DcMotorSimple.Direction.REVERSE);
         (blDrive = hw.blDrive).setDirection(DcMotorSimple.Direction.REVERSE);
@@ -46,96 +42,34 @@ public class Drivetrain {
         resetIMU();
     }
 
-    public void drive(double y, double x, double rx, boolean fieldRelative) {
-        y  = deadband(y);
-        x  = deadband(x) * 1.1;
-        rx = deadband(rx);
+		// normalizes speed (meters per second) to a value from 0 - 1
+		// makes it viable to use for DcMotor.setPower()
+		public float NormalizeSpeed(float speed) {
+			float WHEEL_RADIUS = (float)WHEEL_DIAMETER / 2;
+			float RPM = 312;
+			return 2 * (float)Math.PI * WHEEL_RADIUS / 60 * RPM;
+		}
+		
+		// updates the speed of a motor
+		// uses normalizespeed func to normalize it to be 0 - 1
+		public void UpdateSpeed(DcMotor drive, float speed) {
+			drive.setPower(NormalizeSpeed(speed));
+			return;
+		}
 
-        if (fieldRelative) {
-            double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-            double cos = Math.cos(-heading);
-            double sin = Math.sin(-heading);
-            double rotX = x * cos - y * sin;
-            double rotY = x * sin + y * cos;
-            x  = rotX;
-            y  = rotY;
-        }
+		// updates the direction of a motor
+		public void UpdateDirection(DcMotor drive, DcMotorSimple.Direction direction) {
+			drive.setDirection(direction);
+			return;
+		}
 
-        double fl = (y + x + rx);
-        double fr = (y - x - rx);
-        double bl = (y - x + rx);
-        double br = (y + x - rx);
+		public void resetIMU() {
+			imu.initialize(new IMU.Parameters(
+					new RevHubOrientationOnRobot(
+						RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+						RevHubOrientationOnRobot.UsbFacingDirection.DOWN
+				)
+			));
+		}
 
-        double max = Math.max(1.0, Math.max(Math.abs(fl),
-                         Math.max(Math.abs(fr),
-                         Math.max(Math.abs(bl), Math.abs(br)))));
-
-        flDrive.setPower((fl / max) * speedMultiplier);
-        frDrive.setPower((fr / max) * speedMultiplier);
-        blDrive.setPower((bl / max) * speedMultiplier);
-        brDrive.setPower((br / max) * speedMultiplier);
-    }
-
-    public void setSpeedMultiplier(boolean slow) {
-        speedMultiplier = (slow ? SPEED_SLOW : SPEED_NORMAL);
-    }
-
-    public double getSpeedMultiplier() {
-        return speedMultiplier;
-    }
-
-    public void resetIMU() {
-        imu.initialize(new IMU.Parameters(
-                new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                        RevHubOrientationOnRobot.UsbFacingDirection.DOWN
-                )
-        ));
-    }
-
-    private double deadband(double v) {
-        return Math.abs(v) < STICK_DEADBAND ? 0 : v;
-    }
-
-    public void stop() {
-        for (DcMotor m : new DcMotor[]{flDrive, frDrive, blDrive, brDrive}) {
-            m.setPower(0);
-        }
-    }
-
-    // async + non blocking methods (iterative opmodes, state machines (need for pedro later))
-    public void setDriveTarget(double distance) {
-        int target = (int)(distance * COUNTS_PER_CM);
-
-        for (DcMotor m : new DcMotor[]{flDrive, frDrive, blDrive, brDrive}) {
-            m.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            m.setTargetPosition(target);
-            m.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            m.setPower(AUTO_DRIVE_SPEED);
-        }
-    }
-
-    public boolean isBusy() {
-        // Returns true if ANY motor is still driving to its target
-        return flDrive.isBusy() || frDrive.isBusy() || blDrive.isBusy() || brDrive.isBusy();
-    }
-
-    public void clearDriveTarget() {
-        for (DcMotor m : new DcMotor[]{flDrive, frDrive, blDrive, brDrive}) {
-            m.setPower(0);
-            m.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
-    }
-
-    // synchronous / blocking method (best for linearopmodes)
-    public void driveDistanceBlocking(double distance, LinearOpMode opMode) {
-        setDriveTarget(distance);
-        
-        while (opMode.opModeIsActive() && isBusy()) {
-            // Yield to other processes while waiting
-            opMode.idle(); 
-        }
-        
-        clearDriveTarget();
-    }
 }
