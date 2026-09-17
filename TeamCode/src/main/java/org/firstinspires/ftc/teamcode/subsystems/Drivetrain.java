270e403fa328fcafc6be4061e1116923553c7980
kinglob <der1010101013@gmail.com>
2026-09-17 14:47:54 +1000

    changes to drivetrain
    Signed-off-by: kinglob <der1010101013@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -10,8 +10,6 @@ import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-import org.firstinspires.ftc.teamcode.configs.RobotHardware;
-
 public class Drivetrain {
 
     static final double WHEEL_DIAMETER = 10.4;
@@ -20,8 +18,10 @@ public class Drivetrain {
     private static final double SPEED_SLOW   = 0.4;
     private static final double SPEED_NORMAL = 1;
 
-    private static final double STICK_DEADBAND = 0.05;
+		public double Expected_X = 0;
+		public double Expected_Y = 0;
 
+<<<<<<< Updated upstream
     public static double AUTO_DRIVE_SPEED = 0.4;
 
 		public double Expected_X = 0;
@@ -151,6 +151,12 @@ public class Drivetrain {
 
     private static final double SPEED_SLOW   = 0.4;
     private static final double SPEED_NORMAL = 1;
+=======
+		// tracks the elapsed time since the drive() function has been called
+		// gets set to 0 when using Shift functions so beware
+		public double PrevLastMoved = 0;
+		public double LastMoved = 0;
+>>>>>>> Stashed changes
 
     private static final double STICK_DEADBAND = 0.05;
 
@@ -185,9 +191,19 @@ public class Drivetrain {
 			return power > 1 ? 1 : power;
 		}
 
+		// converts power to speed (meters poer second)
+		private float PowerToSpeed(float power) {
+			float WHEEL_RADIUS = (float)WHEEL_DIAMETER / 2;
+			float speed = (float)Math.PI * WHEEL_RADIUS / 60 * (float)GOBILDA_5203_312RPM * power;
+			return speed;
+		}
+		
 		// takes in joystick input and applies to all motors
 		// thanks omar and kiaan
 		public void drive(double y, double x, double rx, boolean fieldRelative) {
+				PrevLastMoved = LastMoved;
+				LastMoved = System.currentTimeMillis();
+
         y  = deadband(y);
         x  = deadband(x) * 1.1;
         rx = deadband(rx);
@@ -211,10 +227,19 @@ public class Drivetrain {
                          Math.max(Math.abs(fr),
                          Math.max(Math.abs(bl), Math.abs(br)))));
 
-        flDrive.setPower((fl / max) * speedMultiplier);
-        frDrive.setPower((fr / max) * speedMultiplier);
-        blDrive.setPower((bl / max) * speedMultiplier);
-        brDrive.setPower((br / max) * speedMultiplier);
+				double flSpeed = (fl / max) * speedMultiplier;
+        flDrive.setPower(flSpeed);
+
+				double frSpeed = (fr / max) * speedMultiplier;
+        frDrive.setPower(frSpeed);
+
+				double blSpeed = (bl / max) * speedMultiplier;
+        blDrive.setPower(blSpeed);
+
+				double brSpeed = (br / max) * speedMultiplier;
+        brDrive.setPower(brSpeed);
+
+				// find a way to update Expected_X and Expected_Y based on the movement of the drive
     }
 
 		private double deadband(double value) {
@@ -224,8 +249,16 @@ public class Drivetrain {
 		// shifts the robot forward / in reverse based off direction
 		// default is forward
 		public void Shift(float speed, DcMotorSimple.Direction dir = DcMotorSimple.Direction.FORWARD) {
+			PrevLastMoved = LastMoved;
+			LastMoved = System.currentTimeMillis();
 			flDrive.setDirection(dir);
 			frDrive.setDirection(dir);
+
+			if (dir == DcMotorSimple.Direction.FORWARD) {
+				Expected_Y += speed;
+			} else {
+				Expected_Y -= speed;
+			}
 
 			float power = NormalizeSpeed(speed);
 			flDrive.setPower(power);
@@ -235,8 +268,12 @@ public class Drivetrain {
 
 		// updates the movement of the robot to be to their right
 		public void ShiftRight(float speed) {
+			PrevLastMoved = LastMoved;
+			LastMoved = System.currentTimeMillis();
 			frDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 			brDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+			
+			Expected_X += speed;
 
 			float power = NormalizeSpeed(speed);
 			frDrive.setPower(power);
@@ -246,8 +283,12 @@ public class Drivetrain {
 
 		// updates the movement of the robot to be their left
 		public void ShiftLeft(float speed) {
+			PrevLastMoved = LastMoved;
+			LastMoved = System.currentTimeMillis();
 			flDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 			blDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+
+			Expected_X -= speed;
 
 			float power = NormalizeSpeed(speed);
 			flDrive.setPower(power);
